e75179f356528e58ae7597514e871cc9af0fc431
Pineapple Season <emsthai21@gmail.com>
2021-04-13 13:02:35 -0700

    defined query and mutation types

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,3 @@
 // Define the query and mutation functionality to work with the Mongoose models.
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
